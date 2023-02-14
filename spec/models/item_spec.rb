b14f7b75d91fe8a6939fae274018d1944dcdde62
Barsbarloo <lootanke@gmail.com>
2023-02-14 22:06:17 +0600

    Add test to models/item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to :list }
+  it { is_expected.to validate_presence_of(:title) }
 end
