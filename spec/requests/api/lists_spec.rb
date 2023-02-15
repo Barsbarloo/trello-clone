b0ee9881ac8a9b5811b69c98290f1b90503de4d3
Barsbarloo <lootanke@gmail.com>
2023-02-15 10:43:14 +0600

    Add test for api/lists_controller

--- spec/requests/api/lists_spec.rb
+++ spec/requests/api/lists_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "Api::Lists", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) {create(:user) }
+  let(:board) {create(:board, user: user) }
+  let!(:lists) { create_list(:list, 3, board: board) }
+
+  describe "GET index" do
+    it "succeeds" do
+      get api_board_lists_path(board)
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)["data"].size).to eq(3)
+    end
   end
 end
