32a992c092e03a011f61754eb796051d98b396e6
Barsbarloo <lootanke@gmail.com>
2023-02-15 09:58:39 +0600

    fix naming issue from boards to lists

---
+++ app/controllers/api/lists_controller.rb
@@ -0,0 +1,7 @@
+module Api
+  class ListsController < ApplicationController
+    def index
+      
+    end
+  end
+end
--- spec/requests/api/lists_spec.rb
+++ spec/requests/api/lists_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "Api::Boards", type: :request do
+RSpec.describe "Api::Lists", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
