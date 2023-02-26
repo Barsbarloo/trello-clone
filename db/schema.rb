8cf1e1686889e44fafb0d1bebc66398991ab952a
Barsbarloo <lootanke@gmail.com>
2023-02-26 23:07:04 +0600

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_21_101955) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_26_165954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_101955) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
     t.index ["board_id"], name: "index_lists_on_board_id"
   end
 
