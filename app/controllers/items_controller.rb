4e06cd3e15f9668591435b52c9138949023eecb4
Barsbarloo <lootanke@gmail.com>
2023-03-19 23:10:44 +0600

    add def destroy to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,15 +31,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  #   @list = board.lists.find(params[:id])
-  #   @list.destroy
-  #   respond_to do |format|
-  #     format.json do
-  #       render json: {}, status:200
-  #     end
-  #   end
-  # end
+  def destroy
+    @item = list.items.find(params[:id])
+    @item.destroy
+    redirect_to board_path(list.board)
+  end
 
   private
 
