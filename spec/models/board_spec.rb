a2e28512e1b2f9848db335c81781d674039c2e86
Barsbarloo <lootanke@gmail.com>
2023-03-22 23:13:20 +0600

    fix some syntax error

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -5,5 +5,5 @@ RSpec.describe Board, type: :model do
   it { is_expected.to validate_presence_of(:name)}
   it { is_expected.to have_many(:lists).dependent(:destroy) }
   it { is_expected.to have_many(:board_users).dependent(:destroy) }
-  it { is_expected.to have_many(:members).through(:board_users).class_name("User") }
+  it { is_expected.to have_many(:members).through(:board_users).source(:user) }
 end
