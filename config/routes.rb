3cfb27ad0349ff36ae3002933835e65c273b1f04
Barsbarloo <lootanke@gmail.com>
2023-04-06 22:08:56 +0600

    update routes for board users

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :boards do
     resources :lists, except: :show
-    resources :board_users, except: [:show, :update]
+    resources :board_users, only: [:new, :create]
   end
 
   resources :lists do
