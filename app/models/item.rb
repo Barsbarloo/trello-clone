9386be53998f199cdb1cb6bd9044487fcb8a1031
Barsbarloo <lootanke@gmail.com>
2023-04-18 23:34:26 +0600

    add has many associations to items model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   belongs_to :list
 
   validates :title, presence: true
+
+  has_many :item_users, dependent: :destroy
+  has_many :members, through: :item_users, source: :user
 end
