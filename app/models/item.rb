047ae89bbc304857a40377c6f4f6a70776a19e48
Barsbarloo <lootanke@gmail.com>
2023-04-19 23:49:31 +0600

    fix error in naming in item.rb model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,6 @@ class Item < ApplicationRecord
 
   validates :title, presence: true
 
-  has_many :item_users, dependent: :destroy
-  has_many :members, through: :item_users, source: :user
+  has_many :item_members, dependent: :destroy
+  has_many :members, through: :item_members, source: :user
 end
